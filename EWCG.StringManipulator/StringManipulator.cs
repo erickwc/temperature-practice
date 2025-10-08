0d45e989224f2e541dea30379b7eca3b672c533a
erickwc <erickcaceres1717@gmail.com>
2025-10-08 10:29:50 -0600

    Creacion de la clase StringManipulator

---
+++ EWCG.StringManipulator/StringManipulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECRC.StringManipulator
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            return string.IsNullOrEmpty(input) ? input : input.Replace(" ", "");
+        }
+    }
+}
--- EWCG.StringManipulator/obj/Debug/net8.0/EWCG.StringManipulator.AssemblyInfo.cs
+++ EWCG.StringManipulator/obj/Debug/net8.0/EWCG.StringManipulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EWCG.StringManipulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7c7be445b1fc14556e1076f176b3e15bac478c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a447021a8b8a407af7efe99afe6e28e6e10cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EWCG.StringManipulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EWCG.StringManipulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

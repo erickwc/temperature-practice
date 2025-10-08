0e58cd57d24114f549b65a9e418ba92f46227d14
erickwc <erickcaceres1717@gmail.com>
2025-10-08 10:00:33 -0600

    Ejercicio FahrenheitToCelsius agregado

--- EWCG.TemperaturaConverter/TemperatureConverter.cs
+++ EWCG.TemperaturaConverter/TemperatureConverter.cs
@@ -12,5 +12,10 @@ namespace ECRC.Temperatura
         {
             return (celsius * 9 / 5) + 32;
         }
+
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
     }
 }
